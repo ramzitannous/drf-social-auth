ccbd82a7fefcfa3399f675e0b6118832f609ec49
Ramzi Tannous <rtannous@twistbioscience.com>
2020-03-09 16:56:26 +0200

    fix max_length

--- drf_social/serializers.py
+++ drf_social/serializers.py
@@ -7,13 +7,13 @@ except ImportError:
     raise ImportError("must install rest_framework_simplejwt first")
 
 class SocialInputSerializer(serializers.Serializer):
-    access_token = serializers.CharField(max_length=300, required=True, allow_blank=False, allow_null=False)
+    access_token = serializers.CharField(max_length=500, required=True, allow_blank=False, allow_null=False)
     provider = serializers.ChoiceField(choices=[(p, p)for p in models.Providers], allow_null=False, allow_blank=False, required=True)
     client_id = serializers.CharField(max_length=100,allow_null=False, allow_blank=False, required=True)
 
 class JWTResponseSerializer(serializers.Serializer):
-    access = serializers.CharField(max_length=300)
-    refresh = serializers.CharField(max_length=300)
+    access = serializers.CharField(max_length=500)
+    refresh = serializers.CharField(max_length=500)
 
     @classmethod
     def get_token(cls, user):
