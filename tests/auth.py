d06a36a9f1c5e44857c24eb7760939e30a1ebb58
Ramzi Tannous <ramzi271996@gmail.com>
2020-09-26 12:41:15 +0300

    remove token

--- tests/auth.py
+++ tests/auth.py
@@ -5,5 +5,4 @@ class FaceBookTest(APITestCase):
     def test_authenticate(self):
         res = self.client.post("social/login", data={
             "provider": "FACEBOOK",
-            "access_token": "EAAFm5syZAZCy4BAIjyNBfGuL1HQA8RqtkxqXBGaCotchdSNktRhvpKIyY5vR5V0gIRQK4NpjWHbMHMiBUzI4ml2S388Y7j7YVfPORO7mqDQrRDJ55DqtPRnYnFG99ojbrpzYF4gDkWZBZA7groiZCteZCbTdgrbZBcbuQXZCz3QG3jw6TUTxdhMW"
-        })
+            "access_token": ""})
